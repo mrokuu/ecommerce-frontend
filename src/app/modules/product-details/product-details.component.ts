4cc015f1936f6ffc2f87f635bb09e9109209cd9a
mrokuu <mroczkowskiadammm@gmail.com>
2023-07-18 15:58:03 +0200

    reviews

--- src/app/modules/product-details/product-details.component.ts
+++ src/app/modules/product-details/product-details.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductDetails } from './model/productDetails';
 import { ProductDetailsService } from './product-details.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Review } from './model/review';
 
 @Component({
   selector: 'app-product-details',
@@ -10,20 +13,66 @@ import { ProductDetailsService } from './product-details.service';
 })
 export class ProductDetailsComponent implements OnInit {
 
+  // product!: ProductDetails;
+
+  // constructor(
+  //   private productDetailsService: ProductDetailsService,
+  //   private router: ActivatedRoute
+  //   ) { }
+
+  // ngOnInit(): void {
+  //   this.getProductDetails();
+  // }
+
+  // getProductDetails() {
+  //   let id = this.router.snapshot.params['id'];
+  //   this.productDetailsService.getProductDetails(id)
+  //     .subscribe(product => this.product = product);
+  // }
+
+
   product!: ProductDetails;
+  reviewForm!: FormGroup;
 
   constructor(
     private productDetailsService: ProductDetailsService,
-    private router: ActivatedRoute
-    ) { }
+    private router: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
     this.getProductDetails();
+    this.reviewForm = this.formBuilder.group({
+      authorName: ['', [Validators.required, Validators.minLength(2)]],
+      content:  ['', [Validators.required, Validators.minLength(4)]],
+    });
   }
 
   getProductDetails() {
     let id = this.router.snapshot.params['id'];
     this.productDetailsService.getProductDetails(id)
       .subscribe(product => this.product = product);
+  }
+
+  submit() {
+    if(this.reviewForm.valid){
+      this.productDetailsService.saveProductReview({
+        authorName: this.reviewForm.get("authorName")?.value,
+        content:  this.reviewForm.get("content")?.value,
+        productId: this.product.id
+      } as Review).subscribe(review => {
+        this.reviewForm.reset();
+        this.snackBar.open('Thank you for your opinion', '', { duration: 3000, panelClass: "snack-bar-bg-color-ok" });
+      });
+    }
+  }
+
+  get authorName() {
+    return this.reviewForm.get('authorName');
+  }
+
+  get content() {
+    return this.reviewForm.get('content');
   }
 }
